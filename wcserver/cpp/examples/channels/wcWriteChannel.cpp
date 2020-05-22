a23c6e645373c48a1e53ad8d22a8f4289af456d5
hector santos <hector@alpha.winserver.com>
2020-05-22 10:35:22 -0400

    updated cpp\examples\Channels

--- wcserver/cpp/examples/channels/wcWriteChannel.cpp
+++ wcserver/cpp/examples/channels/wcWriteChannel.cpp
@@ -1,14 +1,28 @@
-// File: D:\wc5beta\wcWriteChannel.cpp
+// File:  wcWriteChannel.cpp
+// About: Custom Channel Example
 
 #include <stdio.h>
 #include <afx.h>
-#include <wctype.h>
-#include <wcserver.h>
 #include <conio.h>
+#include <wcserver.h>
+#include <wclinker.h>
 
-#pragma comment(lib,"wcsrv2.lib")
+//////////////////////////////////////////////////////////////
+// Wildcat! process that creates the given channel name
 
-///////////////////////////////////////////////////////////////
+char *pszChProc = "wcProject.exe";    // Process to receive signal
+char *pszChName = "wcsession.viewer"; // custom channel name
+DWORD MY_SIGNAL = 1;                  // custom channel signal
+
+// Custome channel data to write/send to process
+
+typedef struct _TMyChannelData {
+    char s1[80];
+    char s2[80];
+} TMyChannelData;
+
+
+//////////////////////////////////////////////////////////////
 // GetWildcatClientIdByName()
 
 DWORD GetWildcatClientIdByName(const char *szProgamName)
@@ -16,44 +30,49 @@ DWORD GetWildcatClientIdByName(const char *szProgamName)
     TConnectionInfo ci;
     ci.ConnectionId = 0;
     while (GetConnectionInfo(ci.ConnectionId+1, ci)) {
-        if (stricmp(ci.ProgramName,szProgamName) == 0) {
+        if (_stricmp(ci.ProgramName,szProgamName) == 0) {
             return ci.ConnectionId;
         }
     }
     return 0;
 }
 
-typedef struct _TMyChannelData {
-    char s1[80];
-    char s2[80];
-} TMyChannelData;
+//////////////////////////////////////////////////////////////
+// Main Process
+//////////////////////////////////////////////////////////////
 
 void main(char argc, char *argv[])
 {
-  if (!WildcatServerConnect(NULL)) return;
-  printf("1");getch();
-  if (!WildcatServerCreateContext()) return;
-  printf("2");getch();
+    if (!WildcatServerConnect(NULL)) return;
+    if (!WildcatServerCreateContext()) return;
 
-  __try {
-    if (!LoginSystem()) return;
-  printf("3");getch();
-//    DWORD cid = GetWildcatClientIdByName("VB6.EXE");
-    DWORD cid = GetWildcatClientIdByName("projects2.EXE");
-    DWORD ch  = OpenChannel("wcsession.viewer");
-    printf("ch: %d cid: %d\n",ch,cid);
-    TMyChannelData cd;
-    ZeroMemory(&cd,sizeof(cd));
-    strcpy(cd.s1,"Hi There!");
-    strcpy(cd.s2,"Can you hear me now!");
-    //if (!WriteChannel(ch,cid,SC_WATCH_REQUEST,&ch,0)) {
-    if (!WriteChannel(ch,cid,1,&cd,sizeof(cd))) {
-        printf("error %08X writechannel(%d,%d,,,,)\n",GetLastError(),ch,cid);
+    __try {
+        if (!LoginSystem()) return;
+
+        DWORD cid = GetWildcatClientIdByName(pszChProc);
+        DWORD ch  = OpenChannel(pszChName);
+
+        printf("* ch: %d cid: %d\n",ch,cid);
+
+        if (cid == 0) {
+            printf("! cid is 0. Process \"%s\" not running?\n", pszChProc);
+            return;
+        }
+
+        TMyChannelData cd;
+        ZeroMemory(&cd,sizeof(cd));
+
+        strcpy(cd.s1,"Hi There!");
+        strcpy(cd.s2,"Can you hear me now!");
+
+        if (!WriteChannel(ch, cid, MY_SIGNAL, &cd, sizeof(cd))) {
+            printf("error %08X writechannel(%d,%d,,,,)\n",GetLastError(),ch,cid);
+        }
+
+        _cprintf("<Press Any Key To Continue>"); _getch();
+
+    } __finally {
+        WildcatServerDeleteContext();
     }
-  printf("4");getch();
-  } __finally {
-    WildcatServerDeleteContext();
-  printf("5");getch();
-  }
 
 }
